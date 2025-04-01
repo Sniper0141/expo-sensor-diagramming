ef55a2f57d60f64edcd33d8fe012f8af98ab31ca
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-04-01 16:19:05 +0200

    fix + better visuals

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { LineChart } from "react-native-chart-kit";
 import { Accelerometer } from 'expo-sensors';
 
 export default function HomeScreen() {
-  const [chartData, setChartData] = useState<number[]>([]);
+  const [chartData, setChartData] = useState<number[]>([0]);
   
   useEffect(() => {
     Accelerometer.setUpdateInterval(1000);
@@ -17,26 +17,36 @@ export default function HomeScreen() {
   }
 
   return (
-    <LineChart 
-      data={{
-        labels: ["0s ago", "5s", "10s", "15s", "20s"],
-        datasets: [
-          {
-            data: chartData
-          }
-        ]
-      }}
-      width={400}
-      height={300}
-      chartConfig={{
-        color: () => `rgba(255, 255, 255, 1)`,
-        style: {
-          margin: 100
-        }
-      }}>
-    </LineChart>
+    <View style={styles.view}>
+      <Text style={styles.title}>Accelerometer</Text>
+      <LineChart 
+        data={{
+          labels: ["0s ago", "5s", "10s", "15s", "20s", "25s", "30s"],
+          datasets: [
+            {
+              data: chartData
+            }
+          ]
+        }}
+        width={400}
+        height={300}
+        chartConfig={{
+          color: () => `rgba(255, 255, 255, 1)`
+        }}
+        style={{marginTop: 100}}>
+      </LineChart>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  view: {
+    marginTop: 100
+  },
+  title: {
+    color: 'white', 
+    textAlign: 'center',
+    fontSize: 50,
+    fontWeight: 'bold'
+  }
 });
