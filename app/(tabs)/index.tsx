6fc741328629fce7e8f9ce5a9d31f5c8b0ed4d90
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2025-04-01 15:57:30 +0200

    use linechart (doesnt work yet)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,40 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { LineChart } from "react-native-chart-kit";
+import { Accelerometer } from 'expo-sensors';
 
 export default function HomeScreen() {
+  const [chartData, setChartData] = useState<number[]>([]);
+  
+  useEffect(() => {
+    Accelerometer.setUpdateInterval(1000);
+    const sub = Accelerometer.addListener(addData);
+    return () => sub.remove();
+  });
+
+  function addData(data: {x:number, y:number, z:number}){
+    setChartData(d => [data.x, ...d].slice(0, 30));
+  }
+
   return (
-    <></>
+    <LineChart 
+      data={{
+        labels: ["0s ago", "5s", "10s", "15s", "20s"],
+        datasets: [
+          {
+            data: chartData
+          }
+        ]
+      }}
+      width={400}
+      height={300}
+      chartConfig={{
+        color: () => `rgba(255, 255, 255, 1)`,
+        style: {
+          margin: 100
+        }
+      }}>
+    </LineChart>
   );
 }
 
